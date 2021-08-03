f05cbf6431816b0c3cd893069c6bf86fe3f11f52
hadia <hadiamohamed.iti@gmail.com>
2021-08-03 19:40:42 +0200

    Bind values

--- app/src/main/java/com/hadia/task/dashcurrency/ui/exchangerates/ExchangeRatesViewModel.kt
+++ app/src/main/java/com/hadia/task/dashcurrency/ui/exchangerates/ExchangeRatesViewModel.kt
@@ -1,5 +1,6 @@
 package com.hadia.task.dashcurrency.ui.exchangerates
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.hadia.task.dashcurrency.base.BaseViewModel
@@ -10,11 +11,14 @@ import com.hadia.task.dashcurrency.ui.exchangerates.adapter.ExchangeRateUiModel
 import kotlinx.coroutines.launch
 
 class ExchangeRatesViewModel(private val exchangeRateRepository: ExchangeRateRepository) : BaseViewModel() {
-    val exchangeRatesDataList = MutableLiveData<List<ExchangeRateUiModel>>()
+    private val _exchangeRatesDataList = MutableLiveData<List<ExchangeRateUiModel>>()
+
+    val exchangeRatesDataList: LiveData<List<ExchangeRateUiModel>>
+        get() = _exchangeRatesDataList
 
     fun loadExchangeRateList(cryptoCurrencies: CryptoCurrencies) {
         viewModelScope.launch {
-            exchangeRatesDataList.value =
+            _exchangeRatesDataList.value =
                 exchangeRateRepository.getExchangeRates(cryptoCurrencies.code).body()?.data?.rates?.mapNotNull {
                     ExchangeRateUiModel(it.key, it.value.toFormattedNumberString())
                 }
